6dfc8989374d93a7c2efdfe06be0642c42af5e35
Thanh Tin <thanhtin@wiserobot.com>
2026-01-07 09:51:36 +0700

    tin cm

--- index.php
+++ index.php
@@ -288,7 +288,7 @@ switch (ENVIRONMENT)
  * -------------------------------------------------------------------
  */
 date_default_timezone_set('Asia/Ho_Chi_Minh');
-define('FOLDER', '/order74/');
+define('FOLDER', '/');
 define('ADMINCP', 'admincp');
 define('API', 'api');
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on'){
@@ -300,14 +300,14 @@ if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on'){
 }
 define('BASEFOLDER', substr(BASEPATH, 0, strlen(BASEPATH) - 8));
 define('PREFIX', '');
-// define('DB_HOST', '103.18.6.49');
-// define('DB_USER', 'cy46zlubofns_admin');
-// define('DB_PASS', 'Thanhtin@9797');
-// define('DB_NAME', 'cy46zlubofns_orderleotea');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'db_leotea');
+define('DB_HOST', '103.18.6.49');
+define('DB_USER', 'cy46zlubofns_admin');
+define('DB_PASS', 'Thanhtin@9797');
+define('DB_NAME', 'cy46zlubofns_orderleotea');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'db_leotea');
 
 //Config DIR Upload
 define('DIR_UPLOAD_PRODUCT', '/assets/uploads/product/');
