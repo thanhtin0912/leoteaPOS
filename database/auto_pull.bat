d199013bae94032574b9ffd153914dd1144badf4
Thanh Tin <thanhtin@wiserobot.com>
2026-01-08 13:53:55 +0700

    tin cm

---
+++ database/auto_pull.bat
@@ -0,0 +1,5 @@
+:: Bước 1: Di chuyển vào đúng thư mục chứa code website
+cd /d "C:\xampp\htdocs"
+
+:: Bước 2: Gọi lệnh pull bằng đường dẫn tuyệt đối
+"C:\Program Files\Git\cmd\git.exe" pull origin master
